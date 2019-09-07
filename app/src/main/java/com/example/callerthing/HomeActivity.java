3e365dedfab9d584055cb09cd07fb2be731ab729
g-jessmuir <e.jessmuir@gmail.com>
2019-09-07 14:16:50 -0400

    added a button with a function to show an overlay

--- app/src/main/java/com/example/callerthing/HomeActivity.java
+++ app/src/main/java/com/example/callerthing/HomeActivity.java
@@ -1,23 +1,36 @@
 package com.example.callerthing;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.provider.Settings;
 import android.view.View;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        if (Settings.canDrawOverlays(this)) {
+            //launchMainService();
+        } else {
+            checkDrawOverlayPermission();
+        }
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -27,6 +40,55 @@ public class HomeActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        FloatingActionButton showOverlay = findViewById(R.id.showOverlay);
+        showOverlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchMainService();
+            }
+        });
     }
 
+    private void launchMainService() {
+        Intent svc = new Intent(this, MainService.class);
+
+        stopService(svc);
+        startService(svc);
+
+        //finish();
+    }
+    public final static int REQUEST_CODE = 10101;
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public void checkDrawOverlayPermission() {
+
+        // Checks if app already has permission to draw overlays
+        if (!Settings.canDrawOverlays(this)) {
+
+            // If not, form up an Intent to launch the permission request
+            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + getPackageName()));
+
+            // Launch Intent, with the supplied request code
+            startActivityForResult(intent, REQUEST_CODE);
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,  Intent data) {
+
+        // Check if a request code is received that matches that which we provided for the overlay draw request
+        if (requestCode == REQUEST_CODE) {
+
+            // Double-check that the user granted it, and didn't just dismiss the request
+            if (Settings.canDrawOverlays(this)) {
+
+                // Launch the service
+                //launchMainService();
+            }
+            else {
+
+                Toast.makeText(this, "Sorry. Can't draw overlays without permission...", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 }
